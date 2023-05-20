42e7fde26f187cdc251a747e8912be2cf68ce0d2
mfyasykur <mf.yasykur@gmail.com>
2023-05-20 13:39:38 +0700

    Clean code

--- app/src/main/java/com/ppb/trabas_access/LoginActivity.kt
+++ app/src/main/java/com/ppb/trabas_access/LoginActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
-import com.ppb.trabas_access.fragments.HomeFragment
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
